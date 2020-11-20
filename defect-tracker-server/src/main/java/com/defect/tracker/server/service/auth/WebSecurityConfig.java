64ce1cb65a68d77cf0643d6f0fdf86fa59fa15f5
unknown <sanpakees@gmail.com>
2020-11-20 13:34:22 +0530

    inMemorydatabase

---
+++ defect-tracker-server/src/main/java/com/defect/tracker/server/service/auth/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.defect.tracker.server.service.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.passwordEncoder(new BCryptPasswordEncoder())
+		.withUser("san")
+		.password("$2y$10$WYI8/0dTM5y.0VZKCEbFWuU1Y39zOx6V3oFj6EZvh6AGXp0T.VQVK")
+		.roles("USER")
+		;
+	}
+//admin//$10$WYI8/0dTM5y.0VZKCEbFWuU1Y39zOx6V3oFj6EZvh6AGXp0T.VQVK
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.anyRequest().authenticated()
+		.and()
+		.httpBasic()
+		;
+	}
+
+}
